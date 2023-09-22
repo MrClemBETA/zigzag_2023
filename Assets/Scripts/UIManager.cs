c7a75b0aa58d319a56c6a6b2df7bf2f0c775868f
Anthony Clemetson <aclemetson@cousd.net>
2023-09-22 10:14:23 -0700

    Saving the initial game to github.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject gameStartPanel;
+    [SerializeField] GameObject gameEndPanel;
+
+    void Start()
+    {
+        GameManager gm = FindObjectOfType<GameManager>();
+        gm.AddEventToGameStart(DisableStartMenu);
+        gm.AddEventToGameEnd(EnableEndMenu);
+    }
+
+    void EnableEndMenu()
+    {
+        gameEndPanel.SetActive(true);
+        FindObjectOfType<ScoreManager>().EndGame();
+    }
+
+    void DisableStartMenu()
+    {
+        gameStartPanel.SetActive(false);
+    }
+}
